88f95bf4f0c0d4b4950c324764f2d3473a1f86dc
Your Name <email@example.com>
2022-06-17 21:04:32 +0800

    Initial commit

---
+++ src/Test.java
@@ -0,0 +1,11 @@
+/**
+ * @Auther: King
+ * @Date: 2022/6/17 - 06 - 17 - 21:03
+ * @Description: PACKAGE_NAME
+ * @version: 1.0
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
